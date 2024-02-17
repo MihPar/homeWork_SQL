821c2b667355ac6074316ea9ed29045aa18b1643
MihPar <9496260@gmail.com>
2024-02-17 15:45:30 +0300

    fot add CheckRefreshTokenForGet in updateLikeStatus

--- src/api/posts/post.controller.ts
+++ src/api/posts/post.controller.ts
@@ -49,7 +49,7 @@ export class PostController {
 
   @Put(':postId/like-status')
   @HttpCode(204)
-  @UseGuards(CheckRefreshTokenForPost)
+  @UseGuards(CheckRefreshTokenForGet)
   async updateLikeStatus(
 	@Param() dto: InputModelClassPostId, 
 	@Body() status: InputModelLikeStatusClass,
